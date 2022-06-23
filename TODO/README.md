fd38e6b1892aa7f2501ae0da1d33d0b87c7c91bc
vikas.t <vikas.t@pepperfry.com>
2022-06-24 00:07:47 +0530

    todo style setup

--- TODO/README.md
+++ TODO/README.md
@@ -1,1 +1,4 @@
+### Reference
+Bootstrap - https://getbootstrap.com/docs/5.2/getting-started/introduction/
+Material UI - ng add @angular/material
 
--- TODO/src/app/app.module.ts
+++ TODO/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
