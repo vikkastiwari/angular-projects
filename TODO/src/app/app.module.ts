0f628a73ee27cccfa3ceef9789095b9684c68dcd
vikas.t <vikas.t@pepperfry.com>
2022-06-26 18:47:42 +0530

    drag drop added

--- TODO/src/app/app.module.ts
+++ TODO/src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TodoComponent } from './todo/todo.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
@@ -13,6 +12,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TODO/src/app/todo/todo.component.ts
+++ TODO/src/app/todo/todo.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { ItemTask } from '../model/tasks';
 
 @Component({
   selector: 'app-todo',
@@ -8,12 +10,37 @@ import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/form
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todoForm!: FormGroup;
+
+  tasks:ItemTask [] = [];
+  inProgress:ItemTask [] = [];
+  done:ItemTask [] = [];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['', [Validators.required]],
+    })
   }
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  drop(event: CdkDragDrop<ItemTask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 
-
+  addTask(){
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done:false,
+    })
+  }
 }
