b5ea7962ad23dcaff8f816f0635e761f43a51610
vikas.t <vikas.t@pepperfry.com>
2022-12-26 23:05:09 +0530

    init

---
+++ dummy-test/src/app/modules/vlp/vlp.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VlpRoutingModule } from './vlp-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VlpRoutingModule
+  ]
+})
+export class VlpModule { }
